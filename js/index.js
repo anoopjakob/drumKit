1608ec0d8697bcd47ea56a93e0814eecdbad094a
Anoop Jacob <anupjakob@gmail.com>
2020-09-05 22:41:13 +0530

    playing sound on website

--- js/index.js
+++ js/index.js
@@ -1,26 +1,8 @@
 var noOfDrumButtons = document.querySelectorAll(".drum").length;
 for (var i = 0; i < noOfDrumButtons; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function() {
-    alert("i got clicked ");
+    var tom_1 = new Audio("sounds/tom-1.mp3");
+    tom_1.play();
+
   })
-}
-
-
-// higher order functions and passing functions as arguments
-
-function add(num1,num2){
-  return num1 + num2;
-}
-function multiply(num1, num2){
-  return num1 * num2;
-}
-function subtract(num1,num2){
-  return num1-num2;
-}
-function divide(num1, num2){
-  return num1/num2;
-}
-
-function calculate(num1, num2, operator){
-  return operator(num1, num2);
 }

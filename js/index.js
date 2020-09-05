2ce397c7b752ec240a5940d84fe10fcf39929ca2
Anoop Jacob <anupjakob@gmail.com>
2020-09-05 22:32:22 +0530

    learning about higher order functions

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
-document.querySelector("button").addEventListener("click",handleClick);
+var noOfDrumButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < noOfDrumButtons; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+    alert("i got clicked ");
+  })
+}
 
-function handleClick(){
-  alert("i got clicked");
+
+// higher order functions and passing functions as arguments
+
+function add(num1,num2){
+  return num1 + num2;
+}
+function multiply(num1, num2){
+  return num1 * num2;
+}
+function subtract(num1,num2){
+  return num1-num2;
+}
+function divide(num1, num2){
+  return num1/num2;
+}
+
+function calculate(num1, num2, operator){
+  return operator(num1, num2);
 }
